9bc2727702f2320b658c6cad2fc45abfbab3f08c
samdonio <spdonio@gmail.com>
2024-05-17 10:31:19 -0500

    Keeping track of a working copy

--- Final Project/Assets/HandDataLogger.cs
+++ Final Project/Assets/HandDataLogger.cs
@@ -1,68 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-using System.IO;
-using UnityEngine.XR.Hands;
-using UnityEngine.XR.Interaction.Toolkit;
-=======
 using UnityEngine.XR;
 using UnityEngine.XR.Hands;
 using System.IO;
 using System;
->>>>>>> fb204e7a8fec62526f4fc7bd8d32e76c946ed977
 
 public class HandDataLogger : MonoBehaviour
 {
     // Start is called before the first frame update
-<<<<<<< HEAD
-
-    // private List<Vector3> handPositions = new List<Vector3>();
-    public XRHand hand;
-    public XRBaseInteractor leftTriggerInteractor;
-    private Vector3 rightHandPosition;
-    private bool isTracking;
-    private string filePath;
-    void Start()
-    {
-        leftTriggerInteractor = GetComponent<XRBaseInteractor>();
-
-        filePath = Application.dataPath + "/hand_data.csv";
-
-        isTracking = false;
-        // if (hand == null) {
-        //     Debug.LogError("Please assign a right hand XRHand object");
-        // }
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (leftTriggerInteractor.IsActivated() && !isTracking) {
-            isTracking = true;
-            if (!File.Exists(filePath)) {
-                File.WriteAllText(filePath, "Time,X,Y,Z\n");
-            }
-        } else if (leftTriggerInteractor.IsActivated() && isTracking) {
-            isTracking = false;
-        }
-
-
-        if (isTracking) {
-            // Vector3 handPosition = hand.transform.position;
-            // SaveToCSV(handPosition);
-            rightHandPosition = XRNodeState.GetFeaturePose(XRNode.RightHand).position;
-        }
-    }
-
-    void SaveToCSV(Vector3 position) {
-
-        return;
-    }
-
-
-=======
     public XRHand rHand;
     XRHandSubsystem m_HandSubsystem;
     public TextMesh xPos;
@@ -230,5 +176,4 @@ public class HandDataLogger : MonoBehaviour
         // activityText.text = "Updating correctly";
 
     }
->>>>>>> fb204e7a8fec62526f4fc7bd8d32e76c946ed977
 }
